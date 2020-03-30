a0e869763bb2116b40939030520bd18e00138e76
yutiansut <yutiansut@qq.com>
2020-03-30 22:31:33 +0800

    ini

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,6 @@ pub mod data;
 pub mod func;
 pub mod default;
 
-pub use crate::account::{Account, BankDetail, Position, Trade, Transfer, QIFI};
+pub use crate::account::{Account, BankDetail, Position, Trade, Transfer, Order, QIFI};
 pub use crate::data::{Bar, DataItem, Tick, L2X};
 pub use crate::func::{from_bson_, from_serde_value, from_str, from_string, to_doc};
